8b6dbce3718600ff38af18b8f15486957c69c686
915marco <915marco@gmail.com>
2022-03-31 18:01:30 -0500

    updated code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+
 
 const printProfileData = profileDataArr => {
     //This.. 
@@ -10,7 +10,7 @@ const printProfileData = profileDataArr => {
     console.log('=====================');
 
     //Is the same as this..
-    profileDataArr.forEach((profileItem => console.log(profileItem));
+    profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
